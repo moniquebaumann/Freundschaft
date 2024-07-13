91a332069164a384d57118011f7e853cec4f57d4
Monique Baumann <160405077+moniquebaumann@users.noreply.github.com>
2024-07-13 22:33:43 +0200

    Update Freundschaft.sol

--- Freundschaft.sol
+++ Freundschaft.sol
@@ -1,1 +1,18 @@
-// tbd
+// SPDX-License-Identifier: GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+// We fund freedom.
+// We stop state criminals.
+// We make crypto cypherpunk again.
+// We love Geo Caching with Geo Cash.
+// We foster Freedom, Justice and Peace.
+// We combine Freedom Education with Geo Caching.
+// We foster sustainable liquidity infrastructures.
+// We separate money from state criminals like religion has been separated from state criminals.
+// We foster ever emerging architectures of freedom by rewarding those who help themselves and others to be free.
+
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Freundschaft is ERC20 {
+    constructor() ERC20("Freundschaft", "FREUND") {
+        _mint(msg.sender, 9000000000 * 10 ** decimals()); 
+    }
+}
